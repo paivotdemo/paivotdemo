e66c153e8e11078f385c2d65b4a8579278d41770
jandrews78 <you@example.com>
2025-03-05 00:42:27 -0500

    Remove unnecessary cvab-app directory

--- cvab-app-revert-19-main/src/components/NoMatchRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-function NoMatchRoute() {
-    return (
-        <Typography variant="h6">Page Not Found!!</Typography>
-    )
-  }
-  
-  export default NoMatchRoute;
